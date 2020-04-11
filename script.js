0768b3191af8774cdd921b471072f1227ce9f252
Yauheni Shypila <evshipilo@gmail.com>
2020-04-11 21:51:19 +0300

    add: change color on switch

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-import { getCardsArray } from './js/cards.js';
+import { cards } from './js/Cards.js';
 
+const applicationState = {
+  isTrain: 'true',
+  isCategoriesPage: 'true',
+};
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  console.log(getCardsArray);
-});
+function changeStateIsTrain() {
+  if (document.querySelector('.checkbox').checked) applicationState.isTrain = false;
+  else applicationState.isTrain = true;
+}
+
+const changeColorOfCategoryCards = () => {
+  if (applicationState.isTrain) {
+    document.querySelectorAll('.categories-card').forEach((el) => {
+      el.classList.remove('play-gradient');
+      el.classList.add('train-gradient');
+    });
+  } else {
+    document.querySelectorAll('.categories-card').forEach((el) => {
+      el.classList.remove('train-gradient');
+      el.classList.add('play-gradient');
+    });
+  }
+};
+
+const addSwitchClickHandler = () => {
+  document.querySelector('.checkbox').addEventListener('click', () => {
+    changeStateIsTrain();
+    changeColorOfCategoryCards();
+  });
+};
+
+window.onload = function () {
+  addSwitchClickHandler();
+};
