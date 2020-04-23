558378408ca39b5f891af2573f90c7ed522c8a4c
Yauheni Shypila <evshipilo@gmail.com>
2020-04-23 13:15:04 +0300

    init: start task

---
+++ speakit/src/app/js/main.js
@@ -0,0 +1,8 @@
+import '../css/style.scss'
+import { sumMy } from './module/sum'
+import M from 'materialize-css/dist/js/materialize.min';
+
+
+//console.log(`welcome from module: ${sumMy(3)(7)}`)
+
+
