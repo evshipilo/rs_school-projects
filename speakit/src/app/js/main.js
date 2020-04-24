3cf3e09f3d15a4e952c5bd2e11f5f23d7708eccc
Yauheni Shypila <evshipilo@gmail.com>
2020-04-24 22:25:15 +0300

    feat: add handler for cards

--- speakit/src/app/js/main.js
+++ speakit/src/app/js/main.js
@@ -1,68 +1,91 @@
 import '../css/style.scss'
+import M from 'materialize-css/dist/js/materialize.min'
 import { sumMy } from './module/sum'
-import M from 'materialize-css/dist/js/materialize.min';
 
-
-//console.log(`welcome from module: ${sumMy(3)(7)}`)
+// console.log(`welcome from module: ${sumMy(3)(7)}`)
 
 const applicationState = {
-  // isTrain: true,
-  // isCategoriesPage: true,
-  // isStartGame: false,
-  // numOfTrainPage: 0,
-  // isFlip: false,
-   difficulty: 0,
-};
+  difficulty: 0
+}
+
+let arrayOfWordsRandom = []
 
 window.onload = function () {
-  // showStartWords();
-  // addCardsClickHandler();
-  // addButtonsClickHandler();
-   addDifficultyClickHandler();
-};
+  renderCards(applicationState.difficulty, getRandomPageNumber())
+  addCardsClickHandler()
+  // addButtonsClickHandler()
+  addDifficultyClickHandler()
+}
 
 function addDifficultyClickHandler() {
-document.querySelector('.pagination').addEventListener('click',evt => {
-  lightSelectedDifficulty(evt);
+  document.querySelector('.pagination').addEventListener('click', (evt) => {
+    lightSelectedDifficulty(evt)
+    renderCards(applicationState.difficulty, getRandomPageNumber())
+  })
+}
 
-});
+function addCardsClickHandler() {
+  document.querySelector('.cards-container').addEventListener('click', (evt) => {
+    lightSelectedCard(evt)
+    // showTranslation()
+    // showImage()
+    // playSound()
+  })
+}
+
+function lightSelectedCard(evt) {
+  document.querySelectorAll('.word-card').forEach((item, num) => {
+    item.classList.remove('blue')
+    item.classList.remove('lighten-4')
+    item.querySelectorAll('*').forEach((it) => {
+      if (evt.target === it) {
+        item.classList.add('blue')
+        item.classList.add('lighten-4')
+      }
+    })
+  })
 }
 
 function lightSelectedDifficulty(evt) {
-document.querySelectorAll('.pagination a').forEach((item,num)=>{
-  item.parentElement.classList.remove('active');
-  if(evt.target.classList.contains(`pagination-${num}`)) {
-    item.parentElement.classList.add('active');
-    applicationState.difficulty=num;
+  document.querySelectorAll('.pagination a').forEach((item, num) => {
+    item.parentElement.classList.remove('active')
+    if (evt.target.classList.contains(`pagination-${num}`)) {
+      item.parentElement.classList.add('active')
+      applicationState.difficulty = num
+    }
+  })
+}
 
+function getRandomPageNumber() {
+  return Math.round(30 * Math.random() - 0.5)
+}
+
+function getArrayOfWordsRandom(arr) {
+  function makeRandomArr() {
+    return Math.random() - 0.5
   }
-});
-
+  return arr.sort(makeRandomArr)
 }
 
-
-//
-// function showStartWords() {
-//  getDifficulty();
-//
-//
-// }
-
-const getWords = async (page, group) => {
-  const url = `https://afternoon-falls-25894.herokuapp.com/words?page=${page}&group=${group}`;
-  const res = await fetch(url);
-  const json = await res.json();
-  console.log(JSON.stringify(json, null, 1));
-};
-
-getWords(2,2);
-
-function getTranslation (word) {
-  const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200423T131720Z.5b6ec330d739656c.c9086fb5348e50291ba432745fd8571fc4c5ecdc&text= ${word} &lang=en-ru`;
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data.text)
-    });
+async function renderCards(difficulty, pageNumber) {
+  const url = `https://afternoon-falls-25894.herokuapp.com/words?page=${pageNumber}&group=${difficulty}`
+  const res = await fetch(url)
+  const json = await res.json()
+  arrayOfWordsRandom = getArrayOfWordsRandom(Array.from(json))
+  //console.log(arrayOfWordsRandom)
+  insertTextInCards()
 }
-getTranslation('lake');
+
+function insertTextInCards() {
+  for (let i = 0; i < 10; i += 1) {
+    document.querySelector(`.card${i} .word`).innerHTML = arrayOfWordsRandom[i].word
+    document.querySelector(`.card${i} .transcription`).innerHTML = arrayOfWordsRandom[i].transcription
+  }
+}
+
+async function getTranslation (item, num) {
+  const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200423T131720Z.5b6ec330d739656c.c9086fb5348e50291ba432745fd8571fc4c5ecdc&text= ${item.word} &lang=en-ru`
+  const res = await fetch(url)
+  const json = await res.json()
+  item.translation = json.text[0]
+}
