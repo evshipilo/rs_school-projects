7696002acbbbc0082af5e75afab53a91c2d83eff
Yauheni Shypila <evshipilo@gmail.com>
2020-05-04 23:32:53 +0300

    feat: add slider

--- movie-search/src/app/js/main.js
+++ movie-search/src/app/js/main.js
@@ -1,6 +1,15 @@
 import '../css/style.scss';
-import M from 'materialize-css/dist/js/materialize.min';
+import Swiper from 'swiper';
+
+// import M from 'materialize-css/dist/js/materialize.min';
 // import { sumMy } from './module/sum';
 // console.log(`welcome from module: ${sumMy(3)(7)}`);
 
-
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
