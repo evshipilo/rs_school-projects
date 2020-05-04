584639ef27c99cd8070c0a51e993b81d8ce38834
Yauheni Shypila <evshipilo@gmail.com>
2020-05-04 21:22:36 +0300

    init: start task

---
+++ movie-search/src/app/js/main.js
@@ -0,0 +1,6 @@
+import '../css/style.scss';
+import M from 'materialize-css/dist/js/materialize.min';
+// import { sumMy } from './module/sum';
+// console.log(`welcome from module: ${sumMy(3)(7)}`);
+
+
