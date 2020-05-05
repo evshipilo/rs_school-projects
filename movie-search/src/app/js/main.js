7d0ddf4a0890360c6b5b222dac2734092ff60343
Yauheni Shypila <evshipilo@gmail.com>
2020-05-06 01:06:14 +0300

    feat: add translate

--- movie-search/src/app/js/main.js
+++ movie-search/src/app/js/main.js
@@ -20,10 +20,10 @@ const swiper = new Swiper('.swiper-container', {
   },
 });
 
-const appState = {
-  searchValue: '',
-  translation: '',
-};
+// const appState = {
+//   searchValue: '',
+//   translation: '',
+// };
 const formSearch = document.forms[0];
 
 window.onload = function () {
@@ -41,48 +41,85 @@ function addClearClickHandler() {
 function addFindClickHandler() {
   document.querySelector('form').onsubmit = function (event) {
     event.preventDefault();
-    appState.searchValue = formSearch.elements.search.value;
-    if (appState.searchValue) {
-      showProgress();
-      if (isRussianLetters(appState.searchValue)) {
-        typeMessage('Trying to translate...');
-        getTranslation(appState.searchValue);
+    // appState.searchValue = formSearch.elements.search.value;
+    // if (appState.searchValue) {
+    //   showProgress();
+    //   if (isRussianLetters(appState.searchValue)) {
+    //     typeMessage('Trying to translate...');
+    //     getTranslation(appState.searchValue);
+    //   } else {
+    //     appState.translation = appState.searchValue;
+    //     typeMessage(`Looking for "${appState.translation}"`);
+    //     console.log('1');
+    //     const rr = getMovieTitle(1, appState.translation);
+    //     console.log(rr);
+    //   }
+    // } else {
+    //   typeMessage('Empty query, type movie title and press FIND!');
+    // }
+    const searchValue = formSearch.elements.search.value;
+    showFilms(searchValue);
+  };
+}
+
+
+async function showFilms(searchQuery) {
+  if (searchQuery) {
+    showProgress();
+    if (isRussianLetters(searchQuery)) {
+      typeMessage('Trying to translate...');
+      console.log(searchQuery, '0');
+      const translation = deleteSpaces(await getTranslation(searchQuery));
+      console.log(translation, '0');
+      if (isRussianLetters(translation)) {
+        hideProgress();
+        typeMessage(`Wrong translation result "${translation}", type movie title and press FIND!`);
       } else {
-        appState.translation = appState.searchValue;
-        typeMessage(`Looking for "${appState.translation}"`);
-        console.log('1');
-        getMovieTitle(1, appState.translation);
+        typeMessage(`Looking for "${translation}"`);
+        console.log(translation, '1');
+        const title = await getMovieTitle(1, translation);
+        console.log(title, '1');
       }
     } else {
-      typeMessage('Empty query, type movie title and press FIND!');
+      typeMessage(`Looking for "${searchQuery}"`);
+      console.log(searchQuery, '2');
+      const title = await getMovieTitle(1, searchQuery);
+      console.log(title, '2');
     }
-  };
+  } else {
+    typeMessage('Empty query, type movie title and press FIND!');
+  }
+}
+
+function deleteSpaces(word) {
+  return word.replace(/^\s/, '').replace(/\s$/, '');
 }
 
 async function getTranslation(item) {
   const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200423T131720Z.5b6ec330d739656c.c9086fb5348e50291ba432745fd8571fc4c5ecdc&text= ${item} &lang=ru-en`;
   const res = await fetch(url);
   const json = await res.json();
-  [appState.translation] = json.text;
-  if (isRussianLetters(appState.translation)) {
-    hideProgress();
-    typeMessage(`Wrong translation result "${appState.translation}", type movie title and press FIND!`);
-  } else {
-    typeMessage(`Looking for "${appState.translation}"`);
-    console.log(appState.translation);
-    let rr=await getMovieTitle(1, appState.translation);
-    console.log(rr);
-  }
+  return json.text[0];
+  // [appState.translation] = json.text;
+  // if (isRussianLetters(appState.translation)) {
+  //   hideProgress();
+  //   typeMessage(`Wrong translation result "${appState.translation}", type movie title and press FIND!`);
+  // } else {
+  //   typeMessage(`Looking for "${appState.translation}"`);
+  //   console.log(appState.translation);
+  //   const rr = getMovieTitle(1, appState.translation);
+  //   console.log(rr);
+  // }
 }
 
 async function getMovieTitle(page, title) {
-  console.log('++++++++');
+  console.log(page, title);
   const url = `https://www.omdbapi.com/?s=${title}&page=${page}&apikey=50a9f9f4`;
   const res = await fetch(url);
   const data = await res.json();
-  //console.log(data.Search[0].Title, data);
+  // console.log(data.Search[0].Title, data);
 
-  return data;
+  return data.Search;
 }
 
 // function getMovieTitle(page, title) {
