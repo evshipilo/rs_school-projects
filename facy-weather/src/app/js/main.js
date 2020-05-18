a6eb00947e558e20f481cdfa2e467088ebf9f0cf
Yauheni Shypila <evshipilo@gmail.com>
2020-05-18 22:03:47 +0300

    feat: add background change

--- facy-weather/src/app/js/main.js
+++ facy-weather/src/app/js/main.js
@@ -14,43 +14,51 @@ class App extends React.Component {
       yearTime: 'summer',
       backgroundImgSrc: null
     }
-    this.getBackgroundImage = this.getBackgroundImage.bind(this)
+    this.setBackgroundImage = this.setBackgroundImage.bind(this)
   }
 
-  async getBackgroundImage () {
+  async setBackgroundImage () {
     const url = `https://api.unsplash.com/photos/random?orientation=landscape&per_page=1&query=${this.state.dayTime},${this.state.yearTime}&client_id=36fevMXWtK0E8TRgKchz8t-R_jmbo9kmBVuf8pD-2mk`
-
-    const res = await fetch(url)
-    const data = await res.json()
-    const imgUrl = data.urls.regular
-    const resImg = await fetch(imgUrl)
-    console.log(resImg)
-    const blob = await resImg.blob()
-    const image = new Image()
-    image.src = URL.createObjectURL(blob)
-    console.log(image.src)
-    this.setState({ backgroundImgSrc: image.src })
+    try {
+      const res = await fetch(url)
+      const data = await res.json()
+      const imgUrl = data.urls.regular
+      const resImg = await fetch(imgUrl)
+      const blob = await resImg.blob()
+      const image = new Image()
+      image.src = URL.createObjectURL(blob)
+      this.setState({ backgroundImgSrc: image.src })
+    } catch (e) {
+      this.setState({ backgroundImgSrc: 'img/backgroundDefault.jpg' })
+      console.log('cant fetch data from unsplash.com', e)
+    }
   }
 
-  // https://images.unsplash.com/photo-1559075479-e8da6f6fc3fb?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEzNTYwNH0
   async componentDidMount() {
-    await this.getBackgroundImage()
+    await this.setBackgroundImage()
   }
 
-  render() {
-    return (
-      <Container src={this.state.backgroundImgSrc}/>
-    )
-  }
-}
-
-class Container extends React.Component {
   render() {
     return (
       <div
         className='container'
-        style={{ backgroundImage: `url(${this.props.src})` }}
-      >sdcsdc</div>
+        style={{ backgroundImage: `url(${this.state.backgroundImgSrc})` }}
+      >
+        <ChangeBackgroundButton changeBackground={this.setBackgroundImage}/>
+      </div>
+    )
+  }
+}
+
+class ChangeBackgroundButton extends React.Component {
+  render() {
+    return (
+      <button
+        className='btn'
+        onClick={() => this.props.changeBackground()}
+      >
+        <i className="material-icons">refresh</i>
+      </button>
     )
   }
 }
