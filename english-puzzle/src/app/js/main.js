d0e012a2adfceb94183a152d443fc3495bcccde2
Yauheni Shypila <evshipilo@gmail.com>
2020-06-05 10:42:32 +0300

    feat: add dnd component

--- english-puzzle/src/app/js/main.js
+++ english-puzzle/src/app/js/main.js
@@ -2,32 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import M from 'materialize-css/dist/js/materialize.min'
 import '../css/style.scss'
+import DragNdrop from './module/dragNdrop'
 
-
-/* eslint class-methods-use-this: ["error", { "exceptMethods": ["render"] }] */
+/* eslint class-methods-use-this: [0] */
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      a: 0
     }
-
   }
-
 
   async componentDidMount() {
     M.AutoInit()
   }
 
-  componentWillUnmount() {
-  }
-
   render() {
     return (
-<div> <button className='btn waves-effect'>weoijd</button>
-kjhbkjhbkjhbjkhb</div>
-  )
+      <DragNdrop/>
+    )
   }
 }
 
